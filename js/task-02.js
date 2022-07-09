42e2db05dff899240b468e09a7c0a6fb531564ef
1Anka1 <97912793+1Anka1@users.noreply.github.com>
2022-07-09 18:59:41 +0200

    Done task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const elementsOfIngredients = ingredients.map((el) => {
+  const elementIngredients = document.createElement("li");
+  elementIngredients.textContent = el;
+
+  return elementIngredients;
+});
+ingredientsEl.append(...elementsOfIngredients);
