00f24af5ee568b4852d7e36e16fb3541184d365a
1Anka1 <97912793+1Anka1@users.noreply.github.com>
2022-07-12 19:09:39 +0200

    Taks 9
    Done task 9 with change color background

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const widgetDiv = document.querySelector('.widget');
+const nameOfColor = document.querySelector('.color');
+const bottonEl = document.querySelector('.change-color');
+
+
+bottonEl.addEventListener('click', getChangeColor)
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function getChangeColor() {
+  const color = getRandomHexColor();
+  widgetDiv.style.backgroundColor = color;
+  nameOfColor.textContent = widgetDiv.style.backgroundColor = color;
 }
