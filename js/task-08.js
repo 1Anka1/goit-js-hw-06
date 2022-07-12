d18d4a311256b353f67a0d3f5161940241201c24
1Anka1 <97912793+1Anka1@users.noreply.github.com>
2022-07-12 17:26:04 +0200

    Task 9
    Done task 9, I trie to do it with const form = new FormData, But I haven't figured it out yet.

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit )
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    const data = {
+        email,
+        password ,
+    }
+      if (email === "" || password === "") {
+        return alert("Please fill in all the fields!");
+      }
+      console.log(data);
+      event.currentTarget.reset();
+  }
